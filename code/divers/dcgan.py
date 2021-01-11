5bbab0a4079ab449fdaaf752aff211d72ebc679a
Samuel <samuel.dubuis@netplus.ch>
2021-01-11 15:50:37 +0100

    restart

--- code/divers/dcgan.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # https://stackoverflow.com/questions/35911252/disable-tensorflow-debugging-information
-import tensorflow as tf
-
-
-print("TF Version : "+tf.__version__)
-
